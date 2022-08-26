7a555e37c0a5e25431d6df94c7467ecb56ba4912
mahdi <mahdi.heidari.kia@gmail.com>
2022-08-26 20:04:05 +0430

    Fix frontend env loader after build

---
+++ frontend/env_config_template.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-template-curly-in-string */
+
+window._env_ = {
+  REACT_APP_BACKEND_HOST: '${BACKEND_HOST}',
+  REACT_APP_SSL_ENABLED: '${SSL_ENABLED}',
+};
--- frontend/src/api/base.js
+++ frontend/src/api/base.js
@@ -2,13 +2,15 @@ import { showNotification } from '@mantine/notifications';
 import axios from 'axios';
 import Cookies from 'universal-cookie';
 
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const BACKEND_HOST = window._env_.REACT_APP_BACKEND_HOST;
+const SSL_ENABLED = window._env_.REACT_APP_SSL_ENABLED.toLowerCase() === 'true';
+const HTTP_PROTOCOL = SSL_ENABLED ? 'https://' : 'http://';
+export const API_BASE_URL = HTTP_PROTOCOL + BACKEND_HOST;
 
 axios.interceptors.request.use((request) => {
   request.withCredentials = true;
   const cookies = new Cookies();
-  const token = cookies.get('X-CSRFTOKEN');
-  console.log('token', token);
+  const token = cookies.get('X-JR_CSRFTOKEN');
   if (request.method !== 'GET' && token) {
     request.headers['X-CSRFToken'] = token;
   }
